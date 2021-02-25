b2eb2bf661fbcb41737366ed32cd3cf43b6157dc
Mitch Harris <mitchharris@live.ca>
2021-02-24 16:54:29 -0800

    try out other groups

--- lookup/bgg_api.py
+++ lookup/bgg_api.py
@@ -23,13 +23,42 @@ def get_publisher_id_name_dict(publisher_list):
     return id_name_dict
 
 
+def group_id_to_name(id_list, group_name):
+    """
+    asks the api for names to ids then collects into a dict
+    for one group at a time
+
+    id_list : list of ids given as ints
+    group_name : string (one of [artist, publisher, designer, category, mechanic])
+
+    returns : dictionary
+
+    eg:
+    group_id_to_name([100, 150], "publisher")
+    """
+    id_name_dict = {}
+    publishers_string = [str(x) for x in id_list]
+    publishers_string = ",".join(publishers_string)
+    url = "https://www.boardgamegeek.com/xmlapi/boardgame{}/{}".format(
+        group_name, publishers_string
+    )
+    print(url)
+    resp = requests.get(url)
+    tree = ET.fromstring(resp.content)
+    for idx, val in enumerate(list(tree)):
+        name = val.find("name").text
+        id = id_list[idx]
+        id_name_dict[id] = name
+    return id_name_dict
+
+
 if __name__ == "__main__":
     # url = "https://www.boardgamegeek.com/xmlapi/boardgame/13"
     # resp = requests.get(url)
     # tree = ET.fromstring(resp.content)
 
     pub_list = [100, 150]
-    pubs = get_publisher_id_name_dict(pub_list)
+    pubs = group_id_to_name(pub_list, "publisher")
     print(pubs)
 
     # import pdb
