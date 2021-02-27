4c711f2fc37e3c3f258bd80ea0636e7a2da9fc39
Mitch Harris <mitchharris@live.ca>
2021-02-26 19:31:04 -0800

    vectorize series ops

--- lookup/create_lookup_table.py
+++ lookup/create_lookup_table.py
@@ -17,16 +17,17 @@ def extract_ids_from_column(column):
 
     returns: flat list of ints
     """
-    id_list = []
+
+    def split_string(string):
+        return string.split(",")
 
     st = column.astype("str", copy=False).dropna(
         inplace=False
     )  # some columns were mixed type
 
-    for item in list(st):
-        temp = item.split(",")
-        id_list.extend(temp)
-    return list(set(id_list))
+    strings = st.map(lambda x: x.split(","))
+    id_list = set(strings.explode().values)
+    return id_list
 
 
 def create_chunks(id_list, n):
@@ -45,10 +46,10 @@ def create_chunks(id_list, n):
 
 if __name__ == "__main__":
     df = pd.read_csv("../data/bgg_GameItem.csv")
-    df = df[["bgg_id", "publisher"]]
+    df = df[["bgg_id", "category"]]
 
-    ids = extract_ids_from_column(df["publisher"])
-
-    chunked_list = list(create_chunks(ids, 500))
-    pub_id = id_lookup.group_id_to_name(chunked_list[-1], "publisher")
-    print(pub_id)
+    ids = extract_ids_from_column(df["category"])
+    print(len(ids))
+    # chunked_list = list(create_chunks(ids, 500))
+    # pub_id = id_lookup.group_id_to_name(chunked_list[-1], "publisher")
+    # print(len(pub_id))
